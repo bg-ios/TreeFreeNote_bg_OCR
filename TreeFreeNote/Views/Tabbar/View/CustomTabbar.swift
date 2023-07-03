53890e2d16f9f47d7c69cc9e131cb50f5d0b99a0
Bhargavi <bg.ios@necun.in>
2023-07-03 21:21:29 +0530

    Fixed Tabbar issues

--- TreeFreeNote/Views/Tabbar/View/CustomTabbar.swift
+++ TreeFreeNote/Views/Tabbar/View/CustomTabbar.swift
@@ -16,40 +16,44 @@ struct CustomTabbar: View {
     let action: () -> Void
 
     var body: some View {
-        HStack(spacing: 0) {
-            //Tab bar Button..
-            TabbarItem(animation: animation, title: "Home", image: "Home", tag: 1, selectedTab: $selectedTab)
-            TabbarItem(animation: animation, title: "QR Scan", image: "qrScan", tag: 2, selectedTab: $selectedTab)
-            
-            Button {
-                print("tap on camera")
-            } label: {
-                Image("camera")
-                    .resizable()
-                    .renderingMode(.template)
-                    .aspectRatio( contentMode: .fit)
-                    .frame(width: 24, height: 24)
-                    .padding(20)
-                    .background(Color.green)
-                    .foregroundColor(Color.white)
-                    .clipShape(Circle())
-                //shadows
-                    .shadow(color: Color.black.opacity(0.05), radius: 4, x: 5, y: -5)
+        ZStack {
+            Rectangle()
+                .foregroundColor(Color.gray.opacity(0.05))
+                .cornerRadius(35, corners: [.topLeft, .topRight])
+            HStack(spacing: 0) {
+                //Tab bar Button..
+                TabbarItem(animation: animation, title: "Home", image: "Home", tag: 1, selectedTab: $selectedTab)
+                TabbarItem(animation: animation, title: "QR Scan", image: "qrScan", tag: 2, selectedTab: $selectedTab)
+                
+                Button {
+                    print("tap on camera")
+                } label: {
+                    Image("camera")
+                        .resizable()
+                        .renderingMode(.template)
+                        .aspectRatio( contentMode: .fit)
+                        .frame(width: 24, height: 24)
+                        .padding(20)
+                        .background(Color.green)
+                        .foregroundColor(Color.white)
+                        .clipShape(Circle())
+                    //shadows
+                        .shadow(color: Color.black.opacity(0.05), radius: 4, x: 5, y: -5)
+                }
+                .tag("Camera")
+                .offset(y: -25)
+                
+                TabbarItem(animation: animation, title: "OCR Scan", image: "ocrScan", tag: 3, selectedTab: $selectedTab)
+                TabbarItem(animation: animation, title: "Import", image: "Import", tag: 4, selectedTab: $selectedTab)
             }
-            .tag("Camera")
-            .offset(y: -25)
+            .padding(.top)
+            //decrease extra padding space..
+            .padding(.vertical, -10)
+            .padding(.bottom, getSafeArea().bottom == 0 ? 15 : getSafeArea().bottom)
+            .background(Color.clear)
             
-            TabbarItem(animation: animation, title: "OCR Scan", image: "ocrScan", tag: 3, selectedTab: $selectedTab)
-            TabbarItem(animation: animation, title: "Import", image: "Import", tag: 4, selectedTab: $selectedTab)
         }
-        .padding(.top)
-        //decrease extra padding space..
-        .padding(.vertical, -10)
-        .padding(.bottom, getSafeArea().bottom == 0 ? 15 : getSafeArea().bottom)
-        .background(Rectangle()
-            .foregroundColor(Color.gray.opacity(0.05))
-            .cornerRadius(35, corners: [.topLeft, .topRight])
-                 )
+        .frame(height: 74)
     }
 }
 
