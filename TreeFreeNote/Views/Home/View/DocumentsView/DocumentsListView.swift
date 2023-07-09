d76d87dec5afe4a37890305e5e56c4f8c4f210a0
Bhargavi <bg.ios@necun.in>
2023-07-09 13:13:36 +0530

    Added Document list click action and navigation changes

--- TreeFreeNote/Views/Home/View/DocumentsView/DocumentsListView.swift
+++ TreeFreeNote/Views/Home/View/DocumentsView/DocumentsListView.swift
@@ -11,47 +11,20 @@ struct DocumentsListView: View {
     @State private var documentsArray = documentModelSamples
     
     var body: some View {
-        NavigationView {
-            
-            List(documentsArray) { document in
-                DocumentListCell(document: document)
-                    .padding(.horizontal, -10)
-                    .frame(height: 120)
-                    .background( NavigationLink("", destination: DocumentsDetailedView(documentInfo: document)).opacity(0) )
-//                    .onTapGesture {
-//                        print(document)
-//                    }
-            }.onAppear {
-                UITableView.appearance().separatorStyle = .none
-                UITableViewCell.appearance().selectedBackgroundView = {
-                    let view = UIView()
-                    view.backgroundColor = .blue
-                    return view
-                }()
-            }.onDisappear {
-                UITableView.appearance().separatorStyle = .none
-            }
-            .listStyle(PlainListStyle())
-            
-            
-            /*
-            ScrollView {
-                LazyVStack(alignment: .leading, spacing: 10, pinnedViews: .sectionHeaders) {
-//                    Section {
-                        ForEach(documentsArray) { document in
-                            DocumentListCell(document: document)
-                                .frame(height: 120)
-                                .onTapGesture {
-                                    print(document)
-                                }
-                            Divider()
-                        }
-//                    }
+        
+        ScrollView {
+            LazyVStack(alignment: .leading, spacing: 10, pinnedViews: .sectionHeaders) {
+                ForEach(documentsArray) { document in
+                    NavigationLink {
+                        DocumentsDetailedView(documentInfo: document)
+                    } label: {
+                        DocumentListCell(document: document)
+                            .frame(height: 100)
+                    }
+                    Divider()
                 }
             }
-             */
         }
-        
     }
     
 }
