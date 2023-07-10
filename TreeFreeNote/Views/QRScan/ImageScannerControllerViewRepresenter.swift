5cdb3b4cc1a192359f55fa363b33c8639d0ba7f8
Bhargavi <bg.ios@necun.in>
2023-07-10 19:22:01 +0530

    Added Scanner framework Added swiftUI supporting changes for scanner framework Added back action and scanner result changes

---
+++ TreeFreeNote/Views/QRScan/ImageScannerControllerViewRepresenter.swift
@@ -0,0 +1,60 @@
+//
+//  ImageScannerControllerViewRepresenter.swift
+//  TreeFreeNote
+//
+//  Created by Baby on 10/07/23.
+//
+
+import SwiftUI
+
+struct ImageScannerControllerViewRepresenter: UIViewControllerRepresentable{
+    func makeUIViewController(context: Context) -> ImageScannerController {
+        guard let controller=controller as? ImageScannerController else {
+            return ImageScannerController()
+        }
+        controller.imageScannerDelegate = context.coordinator
+        return controller
+    }
+    
+    func updateUIViewController(_ uiViewController: ImageScannerController, context: Context) {
+        
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        return Coordinator { imageResults in
+            self.didFinishScanningWithResults(imageResults)
+        } didClickOnScannerCancel: {
+            self.didClickOnScannerCancel?()
+        }
+    }
+    
+    let controller:UIViewController?
+    var didFinishScanningWithResults: (_ results: ImageScannerResults) -> Void
+    var didClickOnScannerCancel: (() ->Void)?
+    typealias UIViewControllerType = ImageScannerController
+    
+}
+
+class Coordinator:NSObject, ImageScannerControllerDelegate {
+    
+    var didFinishScanningWithResults: (_ results: ImageScannerResults) -> Void
+    var didClickOnScannerCancel: () -> Void?
+    
+    init(didFinishScanningWithResults: @escaping (_: ImageScannerResults) -> Void, didClickOnScannerCancel: @escaping () -> Void?) {
+        self.didFinishScanningWithResults = didFinishScanningWithResults
+        self.didClickOnScannerCancel = didClickOnScannerCancel
+    }
+    
+    func imageScannerController(_ scanner: ImageScannerController, didFinishScanningWithResults results: ImageScannerResults) {
+        self.didFinishScanningWithResults(results)
+    }
+    
+    func imageScannerControllerDidCancel(_ scanner: ImageScannerController) {
+        self.didClickOnScannerCancel()
+    }
+    
+    func imageScannerController(_ scanner: ImageScannerController, didFailWithError error: Error) {
+        
+    }
+        
+}
